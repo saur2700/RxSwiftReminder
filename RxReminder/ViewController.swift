7e287ff3cba86be5783e1a45bc4e90ce7c61651c
Saurav Kumar <t-saurav5.kumar@paytmmoney.com>
2022-07-26 14:01:45 +0530

    Initial Commit

---
+++ RxReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxReminder
+//
+//  Created by Saurav  Kumar on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
