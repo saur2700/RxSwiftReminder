dc640adf733e1182295fd38f7ea6c1e3d9605553
Saurav Kumar <t-saurav5.kumar@paytmmoney.com>
2022-10-12 10:00:20 +0530

    inintial commit

---
+++ RxReminder/reminderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  reminderTableViewCell.swift
+//  RxReminder
+//
+//  Created by Saurav  Kumar on 27/07/22.
+//
+
+import UIKit
+
+class reminderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
